0cf82bdaa00ae6d3f7a40546a63af7a5cd443df1
llhbum <llhbum@gmail.com>
2021-01-08 14:37:44 +0900

    210108 - 프로그래머스 Lv2 문제 클리어 (lang - Java)

--- src/Programmers/Lv2/_124나라의숫자.java
+++ src/Programmers/Lv2/_124나라의숫자.java
@@ -1,4 +1,20 @@
 package Programmers.Lv2;
 
 public class _124나라의숫자 {
+    public String solution(int n) {
+        String answer = "";
+        String[] s = {"4","1","2"};
+        int x = 0;
+        while(n > 0){
+            x = n % 3;
+            n /= 3;
+            if(x == 0){
+                n--;
+            }
+            answer = s[x] + answer;
+        }
+        return answer;
+    }
+
+
 }
