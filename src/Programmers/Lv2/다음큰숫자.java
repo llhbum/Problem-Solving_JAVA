e6a1be6022d918d1f19c3f030ba4a90862b22fff
llhbum <llhbum@gmail.com>
2021-01-21 17:33:53 +0900

    210121 - 프로그래머스 Lv2 문제 클리어 (lang - Java)

--- src/Programmers/Lv2/다음큰숫자.java
+++ src/Programmers/Lv2/다음큰숫자.java
@@ -2,22 +2,15 @@ package Programmers.Lv2;
 
 public class 다음큰숫자 {
     public int solution(int n) {
-        int answer = 0;
-        return answer;
-
-    }
-
-    public static void main(String[] args) {
-        int n = 15;
         int bitCnt = Integer.bitCount(n);
         int ans = 0;
         for(;;){
-            n++;
+            n ++;
             if(bitCnt == Integer.bitCount(n)){
-                ans=n;
+                ans = n;
                 break;
             }
         }
-        System.out.println(ans);
+        return ans;
     }
 }
