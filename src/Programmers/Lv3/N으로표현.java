62ffab5481e61ed47f3840d3e3f30c004161d541
llhbum <llhbum@gmail.com>
2021-02-01 16:52:59 +0900

    210201 - 프로그래머스 Lv3 문제 클리어 (lang - Java)

---
+++ src/Programmers/Lv3/N으로표현.java
@@ -0,0 +1,17 @@
+package Programmers.Lv3;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class N으로표현 {
+    public int solution(int N, int number) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
