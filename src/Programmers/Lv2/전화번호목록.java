e20f63b7762192fedc2a21c5cf6d0afab2b2ae41
Hyo Beom Lee <llhbum@naver.com>
2021-06-17 11:26:18 +0900

    Update 전화번호목록.java

--- src/Programmers/Lv2/전화번호목록.java
+++ src/Programmers/Lv2/전화번호목록.java
@@ -1,25 +1,17 @@
 package Programmers.Lv2;
 
-public class 전화번호목록 {
-
-    public static boolean solution(String[] phone_book) {
-        boolean answer = true;
-
-        for(int i = 0; i < phone_book.length;i++){
-            for(int j = 0; j< phone_book.length; j++){
-                if(i == j){
-                    continue;
-                }
-                if(phone_book[i].equals(phone_book[j])){
-                    return false;
-                }
-                if(phone_book[i].length() <= phone_book[j].length()){
-                    if(phone_book[i].equals(phone_book[j].substring(0,phone_book[i].length()))){
-                        return false;
-                    }
-                }
+import java.util.*;
+class 전화번호목록 {
+    public boolean solution(String[] phone_book) {        
+        HashMap<String, Integer> hm = new HashMap<>();
+        
+        for(int i = 0; i <phone_book.length; i++) hm.put(phone_book[i],0);
+        
+        for(String key : hm.keySet()){
+            for(int i = 1; i<key.length(); i++){
+                if(hm.containsKey(key.substring(0,i))) return false;
             }
         }
-        return answer;
+        return true;
     }
 }
